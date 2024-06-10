cee5cc3fe663af60fd1deb09ae55e64422cb997f
fernandogballes <Fernando.Garcia18@alu.uclm.es>
2024-06-10 21:05:25 +0200

    settings.py updated

--- Sistema/backend/core/settings.py
+++ Sistema/backend/core/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 import environ
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -17,7 +18,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY', default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Configurar el modo debug en función de una variable de entorno
